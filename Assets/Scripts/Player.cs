2fdb0f3a267c0e523315add87e45f7136c5c1f21
DESKTOP-3RI9R03\ido <games@ido.wtf>
2018-11-18 17:53:13 +0200

    175th 1hgj: Charge

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float power = 10;
+    public float speed = 3;
+    public GameObject sign;
+    public float distance = 1;
+    private float t = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sign.transform.localPosition = distance * CalculatedPower() + Vector3.forward * 0.7f;
+		if (Input.GetMouseButtonUp(0))
+        {
+            ThrowMe();
+        } else if (Input.GetMouseButton(0))
+        {
+            t += Time.deltaTime;
+        }
+	}
+
+    private Vector3 CalculatedPower()
+    {
+        return Vector3.forward * (1 + Mathf.Sin(t * speed));
+    }
+
+    private void ThrowMe()
+    {
+        gameObject.GetComponent<Rigidbody>().AddRelativeForce(CalculatedPower() * power, ForceMode.Impulse);
+        Debug.Log(t);
+        t = 0;
+    }
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+    public void StartMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
